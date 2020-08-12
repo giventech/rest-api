455d183dc35cd8525bdf41fe12b05910469846b6
giventech <giventech@gmail.com>
2020-08-12 15:50:12 +1000

    Added OAS with Swagger

--- src/main/java/com/giventech/teams/api/TeamController.java
+++ src/main/java/com/giventech/teams/api/TeamController.java
@@ -3,8 +3,14 @@ package com.giventech.teams.api;
 
 import com.giventech.teams.entity.Team;
 import com.giventech.teams.service.TeamService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import static  org.springframework.http.MediaType.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,7 +27,7 @@ public class TeamController {
         return new ResponseEntity("It's ok", HttpStatus.OK);
     }
 
-    @GetMapping("/teams")
+    @GetMapping("/team")
     public ResponseEntity<List<Team> > getTeams() {
         List <Team> teams  = teamService.findAllTeams();
         if (teams == null) {
@@ -30,11 +36,33 @@ public class TeamController {
             return ResponseEntity.ok(teams);
         }
     }
-
+    @Operation(summary = "Get All teams", description = "Get All teams ", tags = {"All teams"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success", content = {@Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = Team.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = {@Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ApiError.class))}),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = {@Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ApiError.class))}),
+            @ApiResponse(responseCode = "404", description = "Not found", content = {@Content(mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "422", description = "Unprocessable entity", content = {@Content(mediaType = APPLICATION_JSON_VALUE)}),
+    })
     @PostMapping("/team")
     public Team  getTeams(@RequestBody Team team) {
       return teamService.createTeam(team);
     }
+
+
+    /**
+     * Swagger annotation describes the
+     * @param id
+     * @return
+     */
+    @Operation(summary = "Get team", description = "Find a team ", tags = {"Find team "})
+     @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success", content = {@Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = Team.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = {@Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ApiError.class))}),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = {@Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ApiError.class))}),
+            @ApiResponse(responseCode = "404", description = "Not found", content = {@Content(mediaType = APPLICATION_JSON_VALUE)}),
+            @ApiResponse(responseCode = "422", description = "Unprocessable entity", content = {@Content(mediaType = APPLICATION_JSON_VALUE)}),
+    })
 
     @GetMapping("/team/{id}")
     @ResponseBody
@@ -42,3 +70,5 @@ public class TeamController {
         return teamService.findTeam(id);
     }
 }
+
+
