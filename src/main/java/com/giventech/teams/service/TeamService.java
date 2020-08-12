1f11a290e66facb1b47ca1f6d572e6984aac20a5
giventech <giventech@gmail.com>
2020-08-12 16:12:38 +1000

    Added retry mechanisms

--- src/main/java/com/giventech/teams/service/TeamService.java
+++ src/main/java/com/giventech/teams/service/TeamService.java
@@ -1,13 +1,24 @@
 package com.giventech.teams.service;
 
 import com.giventech.teams.entity.Team;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface TeamService {
+
+    // In case of SQL Exception it it will retry to call the services
+    // backoff is the period by which the retry is attempted again
+    @Retryable(
+            value = { SQLException.class },
+            maxAttempts = 2,
+            backoff = @Backoff(delay = 5000))
     List<Team> findAllTeams();
 
     Team createTeam(Team team);
+
 
     Team findTeam(long id);
 }
