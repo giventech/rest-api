12744936a301399bdfc76ca3e88c2530a68cba74
giventech <giventech@gmail.com>
2020-12-02 10:38:01 +1100

    unit tests saved

--- src/test/java/com/giventech/api/ApiApplicationTests.java
+++ src/test/java/com/giventech/api/ApiApplicationTests.java
@@ -1,13 +1,17 @@
 package com.giventech.api;
 
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.mockito.ArgumentMatchers.any;
+
 
 @SpringBootTest
 class ApiApplicationTests {
 
+    @Ignore()
     @Test
     void contextLoads() {
+        //TODO  Fabrice to implement
     }
-
 }
